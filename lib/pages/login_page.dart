324c4df60752d29cb9251ec072207bcbf866de1a
aylla <ayllatyfanny16@gmail.com>
2020-01-06 17:11:38 -0300

    primeiro commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,103 @@
+import 'package:carros/widgets/app_button.dart';
+import 'package:carros/widgets/app_text.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _tLogin = TextEditingController();
+
+  final _tSenha = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _focusSenha = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            AppText(
+              "Login",
+              "Digite o login",
+              controller: _tLogin,
+              validator: _validateLogin,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              nextFocus: _focusSenha,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            AppText(
+              "Senha",
+              "Digite a senha",
+              password: true,
+              controller: _tSenha,
+              validator: _validateSenha,
+              keyboardType: TextInputType.number,
+              focusNode: _focusSenha,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            AppButton("Login", onPressed: _onClickLogin),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onClickLogin() {
+    bool formOk = _formKey.currentState.validate();
+
+    if (!formOk) {
+      return;
+    }
+
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
+
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite a senha";
+    }
+
+    if (text.length < 3) {
+      return "A senha deve ter pelo menos três números";
+    }
+    return null;
+  }
+}
