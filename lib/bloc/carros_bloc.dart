33c3a9df8cd0cca016ee214d1e4936b17c8beb37
Aylla <ayllatyfanny16@gmail.com>
2020-01-21 20:48:30 -0300

    seção 17

--- lib/bloc/carros_bloc.dart
+++ lib/bloc/carros_bloc.dart
@@ -1,10 +1,20 @@
 import 'package:carros/bloc/simple_bloc.dart';
+import 'package:carros/dao/carro_dao.dart';
 import 'package:carros/models/carro.dart';
 import 'package:carros/services/carros_api.dart';
+import 'package:carros/utils/network.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> fetch(String tipo) async {
     try {
+      bool network = await isNetworkOn();
+
+      if (!network) {
+        List<Carro> carros = await CarroDAO().findAllByTipo(tipo);
+        add(carros);
+        return carros;
+      }
+
       List<Carro> carros = await CarrosApi.getCarros(tipo);
 
       add(carros);
